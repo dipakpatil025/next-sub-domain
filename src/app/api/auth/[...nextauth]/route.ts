ec9da89ba1cad7d4da445834f8c7ce74a22937fe
Dipak Patil <dipak.patil@noovosoft.com>
2024-04-14 00:44:00 +0530

    feat: sub-domain with https

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,20 @@
 import NextAuth from "next-auth";
 
 import { authOptions } from "@/server/auth";
+import { NextApiRequest, NextApiResponse } from "next";
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-const handler = NextAuth(authOptions);
+
+export async function handler(req: NextApiRequest, res: NextApiResponse) {
+
+  // @ts-ignore
+  const host = req.headers.get('host');
+  const urls = host.split('.');
+  const iceoCode = urls.length > 1 ? urls[0] : 'test1';
+  console.log('iceoCode: --> ', { host,urls,iceoCode });
+  process.env['NEXTAUTH_URL'] = `https://${iceoCode}.localhost:3000`;
+
+  return await NextAuth(req, res, authOptions({ iceoCode: iceoCode }))
+}
+
 export { handler as GET, handler as POST };
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -19,10 +19,10 @@ import Cognito from "next-auth/providers/cognito";
 declare module "next-auth" {
   interface Session extends DefaultSession {
     user: {
-      id: string;
-      // ...other properties
-      // role: UserRole;
-    } & DefaultSession["user"];
+            id: string;
+            // ...other properties
+            // role: UserRole;
+          } & DefaultSession["user"];
   }
 
   // interface User {
@@ -36,39 +36,69 @@ declare module "next-auth" {
  *
  * @see https://next-auth.js.org/configuration/options
  */
-export const authOptions: NextAuthOptions = {
-  callbacks: {
-    session: ({ session, user }) => ({
-      ...session,
-      user: {
-        ...session.user,
-        id: user.id,
-      },
-    }),
-  },
-  adapter: PrismaAdapter(db) as Adapter,
-  providers: [
-    Cognito({
-      clientId: env.AUTH_COGNITO_ID,
-      clientSecret: env.AUTH_COGNITO_SECRET,
-      issuer: env.AUTH_COGNITO_ISSUER,
+export const authOptions = ({ iceoCode }: { iceoCode: string }): NextAuthOptions =>
+  ({
+    debug: true,
+    callbacks: {
+      session: ({ session, user }) => ({
+        ...session,
+        user: {
+          ...session.user,
+          id: user.id,
+        },
+      }),
+    },
+    adapter: PrismaAdapter(db) as Adapter,
+    // secret: env.NEXTAUTH_SECRET,
+    providers: [
+      /**
+       * The Discord provider is the simplest to set up. You only need the client ID and secret.
+       *
+       * @see https://next-auth.js.org/providers/cognito
+       */
+      Cognito({
+        clientId: env.AUTH_COGNITO_ID,
+        clientSecret: env.AUTH_COGNITO_SECRET,
+        issuer: env.AUTH_COGNITO_ISSUER,
+        authorization: {
+          params: {
+            redirect_uri: `https://${iceoCode}.localhost:3000/api/auth/callback/cognito`
+          }
+        }
+      }),
+      /**
+       * ...add more providers here.
+       *
+       * Most other providers require a bit more work than the Discord provider. For example, the
+       * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
+       * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
+       *
+       * @see https://next-auth.js.org/providers/github
+       */
+    ],
 
-    }),
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
-  ],
-};
+    // session: {
+    //   maxAge: 30 * 24 * 60 * 60, // 30 days
+    //   updateAge: 24 * 60 * 60, // 24 hours
+    // },
+    cookies: {
+      sessionToken: {
+        name: `next-auth.session-token`,
+        options: {
+          httpOnly: false,
+          sameSite: 'lax',
+          path: '/',
+          domain: `${iceoCode}.localhost`, // replace with your domain, without subdomains
+          secure: process.env.NODE_ENV === 'production', // set to true in production
+        },
+      },
+    },
+  })
+;
 
 /**
  * Wrapper for `getServerSession` so that you don't need to import the `authOptions` in every file.
  *
  * @see https://next-auth.js.org/configuration/nextjs
  */
-export const getServerAuthSession = () => getServerSession(authOptions);
+export const getServerAuthSession = () => getServerSession(authOptions({ iceoCode: 'test2' }));
